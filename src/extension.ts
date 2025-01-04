64a371a217d8ea3696868329a7634346e8b90c3d
Hassanyoung1 <hhassanhakeem@gmail.com>
2025-01-04 13:50:15 -0800

    level1

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  vscode.window.showInformationMessage('Live Share Enhancer is now active!');
+}
+
+export function deactivate() {
+  console.log('Live Share Enhancer has been deactivated.');
+}
+
